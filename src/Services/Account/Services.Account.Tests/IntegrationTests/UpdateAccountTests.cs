ffcbe9060e1b8b73b186d21b64e09a652807fff2
Peter Kneale <peter.kneale@nearmap.com>
2017-01-24 17:12:10 +1100

    add test

--- src/Services/Account/Services.Account.Tests/IntegrationTests/UpdateAccountTests.cs
+++ src/Services/Account/Services.Account.Tests/IntegrationTests/UpdateAccountTests.cs
@@ -70,5 +70,31 @@ namespace Services.Account.Tests
             // assert
             Assert.Equal("Name must be supplied", ex.Message);
         }
+        
+        [Fact]
+        public void OtherAccountsAreNotUpdated()
+        {
+            // arrange
+            var id1 = new Guid("b8d47dce23074128be1d3537b02dcd62");
+            var id2 = new Guid("c2347dce11114128be1d3537b02dcd63");
+            var name = "Account Name";
+            var nameUpdated = "Account Name Updated";
+
+            var createService = _context.Resolve<CreateAccountService>();
+            var updateService = _context.Resolve<UpdateAccountService>();
+            var getService = _context.Resolve<GetAccountService>();
+
+            // act
+            createService.Post(new CreateAccount { Id = id1, Name = name });
+            createService.Post(new CreateAccount { Id = id2, Name = name });
+            updateService.Put(new UpdateAccount { Id = id1, Name = nameUpdated }); // Only update #1
+            var response = getService.Get(new GetAccount { Id = id2 });
+
+            // assert
+            Assert.NotNull(response);
+            Assert.NotNull(response.Account);
+            Assert.Equal(id2, response.Account.Id);
+            Assert.Equal(name, response.Account.Name);   // #2 should be original
+        }
     }
 }
